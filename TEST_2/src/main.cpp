3a0a515a7bcb6971e27556c42dbc3240ff8d18b5
ZipperXYZ <gbosse2008@email.com>
2023-06-08 12:03:16 -0400

    just made all the error temporary now

--- TEST_2/src/main.cpp
+++ TEST_2/src/main.cpp
@@ -14,8 +14,16 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include "MAIN_CLASS.h"
 
 using namespace vex;
+
+DRIVE_TRAIN Drive_train;
+
+int WHEN_STARTED{
+
+    return 0;
+}
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
